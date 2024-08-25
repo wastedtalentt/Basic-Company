469f05bb3cd724be79251ff9cdb49a8ea7888bd3
wastedtalentt <154629776+wastedtalentt@users.noreply.github.com>
2024-08-25 16:09:28 +0400

    Add files via upload

---
+++ Classes.cs
@@ -0,0 +1,149 @@
+﻿namespace BasicCompany
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+
+        protected internal string CoWorkerCard { get; set; }
+
+        private static HashSet<Visitor> invitedVisitors = new HashSet<Visitor>();
+        private static Dictionary<Visitor, Company> visitorInvitedBy = new Dictionary<Visitor, Company>();
+
+        public virtual void LeftFromCompany(List<Company> workerList)
+        {
+            if (workerList.Contains(this))
+            {
+                workerList.Remove(this);
+                Console.WriteLine($"{Name} has left the company.");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} is not part of the company and cannot leave.");
+            }
+        }
+
+        public virtual void JoinInCompany(List<Company> workerList)
+        {
+            if (!workerList.Contains(this))
+            {
+                workerList.Add(this);
+                Console.WriteLine($"{Name} has joined the company.");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} is already part of the company.");
+            }
+        }
+
+        public void InviteVisitor(Visitor visitor)
+        {
+            if (this is CoWorker || this is Manager || this is Specialist || this is BEDeveloper || this is Operator)
+            {
+                if (!invitedVisitors.Contains(visitor))
+                {
+                    invitedVisitors.Add(visitor);
+                    visitorInvitedBy[visitor] = this;
+                    Console.WriteLine($"{Name} invited {visitor.Name} to the company.");
+                }
+                else
+                {
+                    Console.WriteLine($"{visitor.Name} has already been invited.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{Name} is not authorized to invite visitors.");
+            }
+        }
+
+        public bool AllowVisitorToJoin(Visitor visitor, List<Company> companyList)
+        {
+            if (invitedVisitors.Contains(visitor))
+            {
+                visitor.JoinInCompany(companyList);
+                invitedVisitors.Remove(visitor);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"{visitor.Name} was not invited or is not authorized to join.");
+                return false;
+            }
+        }
+    }
+
+    public class CoWorker : Company { }
+
+    public class Manager : Company
+    {
+        public override void JoinInCompany(List<Company> workerList)
+        {
+            base.JoinInCompany(workerList);
+        }
+    }
+
+    public class Specialist : Company
+    {
+        public TimeSpan WorkStart { get; set; }
+        public TimeSpan WorkEnd { get; set; }
+
+        public Specialist()
+        {
+            WorkStart = new TimeSpan(9, 0, 0);
+            WorkEnd = new TimeSpan(17, 0, 0);
+        }
+
+        public Specialist(TimeSpan workStart, TimeSpan workEnd)
+        {
+            WorkStart = workStart;
+            WorkEnd = workEnd;
+        }
+
+        public bool IsWorking(TimeSpan time)
+        {
+            return time >= WorkStart && time <= WorkEnd;
+        }
+
+        public override void JoinInCompany(List<Company> workerList)
+        {
+            base.JoinInCompany(workerList);
+            Console.WriteLine($"{Name} works from {WorkStart} to {WorkEnd}.");
+        }
+    }
+
+    public class BEDeveloper : Specialist { }
+    public class Operator : Specialist { }
+
+    public class Visitor : Company
+    {
+        public new string CoWorkerCard
+        {
+            get
+            {
+                throw new InvalidOperationException("Visitor class cannot access CoWorkerCard.");
+            }
+            set
+            {
+                throw new InvalidOperationException("Visitor class cannot access CoWorkerCard.");
+            }
+        }
+
+        public override void JoinInCompany(List<Company> workerList)
+        {
+            Console.WriteLine("Visitors cannot join the company directly. They need an invite.");
+        }
+
+        public override void LeftFromCompany(List<Company> workerList)
+        {
+            if (workerList.Contains(this))
+            {
+                base.LeftFromCompany(workerList);
+            }
+            else
+            {
+                Console.WriteLine($"{Name} cannot leave the company because they are not a part of it.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using BasicCompany;
+
+namespace myProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Company> workersList = new List<Company>();
+
+            Console.WriteLine("How many co-workers do you want to add to the company?");
+            if (!int.TryParse(Console.ReadLine(), out int NumberOfCoWorkers) || NumberOfCoWorkers <= 0)
+            {
+                Console.WriteLine("Invalid number. Exiting program.");
+                return;
+            }
+
+            for (int i = 1; i <= NumberOfCoWorkers; i++)
+            {
+                workersList.Add(GetWorkerFromInput());                          //workerList (სია) - ში დინამიურად ვამატებ თანამშრომლებს, მომხმარებლის მიერ მითითებული რაოდენობით.
+            }
+
+            Visitor visitor = new Visitor { Name = "Guest" };
+            if (!visitor.AllowVisitorToJoin(visitor, workersList))
+            {
+                Console.WriteLine("Visitor cannot join directly. They need an invite.");
+            }
+
+            Console.WriteLine("Who do you want to invite the visitor from?");
+            Console.WriteLine("Choose a number corresponding to a worker you added:");
+
+            int invitedFrom;
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out invitedFrom) || invitedFrom <= 0 || invitedFrom > workersList.Count)          // მომხმარებელი უთითებს მის მიერ შერჩეული თანამშრომლებიდან ვის მოაწვევინებს ვიზიტორს.
+                {
+                    Console.WriteLine("Invalid choice. Please choose a valid worker index.");                                           // მხოლოდ კომპანიაში დასაქმებულ ადამიანს შეუძლია ვიზიტორის მოწვევა.
+                    continue;
+                }
+                invitedFrom -= 1;       
+                break;
+            }
+
+            Console.WriteLine("Attempting to invite a visitor...");
+            workersList[invitedFrom].InviteVisitor(visitor);                        
+
+            if (visitor.AllowVisitorToJoin(visitor, workersList))
+            {
+                workersList.Add(visitor);
+                Console.WriteLine($"{visitor.Name} has successfully joined the company.");
+            }
+            else
+            {
+                Console.WriteLine($"{visitor.Name} could not join the company.");
+            }
+
+            Console.WriteLine("\nFinal list of workers in the company:");
+            foreach (Company worker in workersList)
+            {
+                Console.WriteLine(worker.Name);
+            }
+
+            Console.WriteLine("Let's reate several objects:");
+
+
+            BEDeveloper beDeveloper = new BEDeveloper { Name = "David" };                                                   //ვქმნით  რამდენიმე ობიექტს
+            beDeveloper.JoinInCompany(workersList);
+            beDeveloper.LeftFromCompany(workersList);
+
+            Manager manager1 = new Manager();
+            Console.WriteLine("Input name");
+            manager1.Name = Console.ReadLine();
+            manager1.JoinInCompany(workersList);
+
+            Operator operator1 = new Operator();
+            Console.WriteLine(" Input name");
+            operator1.Name = Console.ReadLine();
+            operator1.JoinInCompany(workersList);
+
+            Specialist specialist1 = new Specialist();
+            Console.WriteLine("Input name");
+            specialist1.Name = Console.ReadLine();
+            specialist1.JoinInCompany(workersList);
+      
+
+
+            Visitor visitor1 = new Visitor();
+            visitor1.JoinInCompany(workersList);
+
+            Console.WriteLine("The End");
+            Console.WriteLine("Press any key to exit program");
+            Console.ReadKey();
+        }
+
+        static Company GetWorkerFromInput()
+        {
+            while (true)
+            {
+                Console.WriteLine("[1] - Manager\n[2] - CoWorker\n[3] - Specialist\n[4] - BEDeveloper\n[5] - Operator");
+                if (!int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        return new Manager { Name = GetString("Enter Manager Name") };
+                    case 2:
+                        return new CoWorker { Name = GetString("Enter CoWorker Name") };
+                    case 3:
+                        return new Specialist { Name = GetString("Enter Specialist Name") };
+                    case 4:
+                        return new BEDeveloper { Name = GetString("Enter BEDeveloper Name") };
+                    case 5:
+                        return new Operator { Name = GetString("Enter Operator Name") };
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        static string GetString(string prompt)
+        {
+            Console.WriteLine("{0} > ", prompt);
+            return Console.ReadLine();
+        }
+    }
+}
